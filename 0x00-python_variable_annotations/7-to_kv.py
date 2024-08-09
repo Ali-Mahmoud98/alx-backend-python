17c5ea6f2cbc4bf3c182f2681d5993c9a08de1e1
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-09 18:08:17 +0300

    Complex types - string and int/float to tupl

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,15 +4,15 @@
 from typing import Union, Tuple
 
 
-def to_kv(key: str, value: Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Square a value and return a tuple with the key and the squared value.
 
     Args:
-        key (str): The key.
-        value (Union[int, float]): The value to square.
+        k (str): The key.
+        v (Union[int, float]): The value to square.
 
     Returns:
         Tuple[str, float]: A tuple with the key and the squared value.
     """
-    return key, float(value ** 2)
+    return k, float(v ** 2)
