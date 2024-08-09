ed544380f0a53ed693e807c1c05bed3558cc8a31
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-09 15:28:12 +0300

    mixed list of int and float, typing(List, Union)

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Sum mixed list function. """
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Sums a list of numbers.
 
     Args:
@@ -13,4 +13,4 @@ def sum_mixed_list(mxd_lst: List[float]) -> float:
     Returns:
         float: The sum of the list of numbers.
     """
-    return sum(mxd_lst)
+    return float(sum(mxd_lst))
