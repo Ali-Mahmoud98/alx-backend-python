61a0ced9fd17186bc2bb1fc5f8c02b7c6755ac09
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-09 15:24:59 +0300

    type-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Sum mixed list function. """
+
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float]) -> float:
+    """Sums a list of numbers.
+
+    Args:
+        input_list (List[float]): The list of numbers to sum.
+
+    Returns:
+        float: The sum of the list of numbers.
+    """
+    return sum(mxd_lst)
