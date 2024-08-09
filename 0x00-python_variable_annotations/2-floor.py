8fec5bbb8e199bf049cfb56f1bcb79782cf42e75
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-09 15:10:24 +0300

    type-annotated function floor which takes a float n

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Floor function. """
+
+
+def floor(n: float) -> int:
+    """Finds the floor of a number.
+
+    Args:
+        n (float): The number to find the floor of.
+
+    Returns:
+        int: The floor of the number.
+    """
+    return int(n)
