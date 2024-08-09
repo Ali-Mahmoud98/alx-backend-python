5af27efdebea5f2a4d39448e788f4af2268eddca
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-09 15:13:04 +0300

    type-annotated function to_str that takes a float n as argument and returns the string representation of the float.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" This module contains the to_str function. """
+
+
+def to_str(n: float) -> str:
+    """Converts a float to a string.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        str: The string representation of the number.
+    """
+    return str(n)
