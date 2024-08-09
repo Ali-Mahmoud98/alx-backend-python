ee549699995dfff18f565082949a2c3f7a0a7ce8
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-09 15:29:00 +0300

    ype-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float.

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -8,7 +8,7 @@ def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Sums a list of numbers.
 
     Args:
-        input_list (List[float]): The list of numbers to sum.
+        mxd_lst (List[Union[int, float]]): The list of numbers to sum.
 
     Returns:
         float: The sum of the list of numbers.
