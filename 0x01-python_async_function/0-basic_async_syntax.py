daa712ce851cf5890187e36c55b718fdf53e1d8b
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-17 14:24:26 +0300

    wait random coroutine

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Return the float number of seconds"""
+    import random
+    import asyncio
+
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
