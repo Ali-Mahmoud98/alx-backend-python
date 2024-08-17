7cd59e8b90020d23830113271c5582e0fe831d0e
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2024-08-17 15:40:51 +0300

    concurrent_coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -12,7 +12,7 @@ wait_random = __import__("0-basic_async_syntax").wait_random
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     """Return the list of all delays"""
-    return [await wait_random(max_delay) for _ in range(n)]
+    return sorted([await wait_random(max_delay) for _ in range(n)])
 
 # async def wait_n(n: int, max_delay: int) -> List[float]:
 #     """doc func"""
